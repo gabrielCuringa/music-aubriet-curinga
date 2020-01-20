d295e0a935bd4fb4233ab1787bba8759fb6efa5c
kevinaubriet <kevin97@orange.fr>
2020-01-20 22:39:18 +0100

    adding play audio

--- src/screens/home/component/listSongs.js
+++ src/screens/home/component/listSongs.js
@@ -10,39 +10,60 @@ import Paper from "@material-ui/core/Paper";
 
 const useStyles = makeStyles({
   table: {
-    minWidth: 650
-  }
+    minWidth: 400
+  },
+  tableHeader: {
+    backgroundColor: "black"
+  },
+  tableCell: { color: "white", fontWeight: "Bold" }
 });
-
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
 
 export default function ListSongs(props) {
   const classes = useStyles();
 
+  const playAudio = function(indexToPlay) {
+    console.log("child : " + indexToPlay);
+    const audioEl = document.getElementsByClassName("audio-element")[
+      indexToPlay
+    ];
+    audioEl.play();
+  };
+
   return (
     <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="simple table">
-        <TableHead>
+      <Table className={classes.table} aria-label="sticky table">
+        <TableHead className={classes.tableHeader}>
           <TableRow>
-            <TableCell>Titre</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell className={classes.tableCell}>Titre</TableCell>
+            <TableCell align="right" className={classes.tableCell}>
+              Rang Deezer
+            </TableCell>
+            <TableCell align="right" className={classes.tableCell}>
+              Date de publication
+            </TableCell>
+            <TableCell align="right" className={classes.tableCell}>
+              Preview
+            </TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {props.list.map(song => (
-            <TableRow key={song.title}>
+          {props.list.map((song, i) => (
+            <TableRow key={i}>
               <TableCell component="th" scope="row">
                 {song.title}
               </TableCell>
-              <TableCell align="right">{"row.calories"}</TableCell>
-              <TableCell align="right">{"row.fat"}</TableCell>
-              <TableCell align="right">{"row.carbs"}</TableCell>
-              <TableCell align="right">{"row.protein"}</TableCell>
+              <TableCell align="right">{song.rank}</TableCell>
+              <TableCell align="right">{song.publicationDate}</TableCell>
+              <TableCell align="right">
+                <div>
+                  <button onClick={() => playAudio(i)}>
+                    <span>Play Audio</span>
+                  </button>
+                  <audio className="audio-element">
+                    <source src={song.preview}></source>
+                  </audio>
+                </div>
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
